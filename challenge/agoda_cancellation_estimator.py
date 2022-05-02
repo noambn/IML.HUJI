1a682a4c772e206abeb55b3d325288cdffa73687
Noam Ben Natan <noam.bennatan@mail.huji.ac.il>
2022-05-02 21:45:52 +0300

    challenge week 4

--- challenge/agoda_cancellation_estimator.py
+++ challenge/agoda_cancellation_estimator.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import NoReturn
 from IMLearn.base import BaseEstimator
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 
 class AgodaCancellationEstimator(BaseEstimator):
@@ -12,63 +13,67 @@ class AgodaCancellationEstimator(BaseEstimator):
     def __init__(self) -> AgodaCancellationEstimator:
         """
         Instantiate an estimator for solving the Agoda Cancellation challenge
-
         Parameters
         ----------
-
-
         Attributes
         ----------
-
         """
         super().__init__()
+        self.coef_ = None
+        self.intercept_ = None
+        self.logistic_regression_obj = LogisticRegression(solver='liblinear', max_iter=500)
+        self.fitted = False
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
         Fit an estimator for given samples
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to fit an estimator for
-
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
-
         Notes
         -----
-
         """
-        pass
+
+        self.model = self.logistic_regression_obj.fit(X, y)
+
+        self.coef_ = self.model.coef_
+        self.intercept_ = self.model.intercept_
+
+        print(self.coef_)
+        print(self.intercept_)
+        print("argmax", self.coef_.argmax())
+        print(X[0, self.coef_.argmax()])
+
+        self.fitted_ = True
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict responses for given samples using fitted estimator
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to predict responses for
-
         Returns
         -------
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        return np.zeros(X.shape[0])
+        # todo raise error if self.fitted is false?
+
+        return self.logistic_regression_obj.predict(X)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
         Evaluate performance under loss function
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Test samples
-
         y : ndarray of shape (n_samples, )
             True labels of test samples
-
         Returns
         -------
         loss : float
